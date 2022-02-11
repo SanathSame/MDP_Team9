e1c6bc4ae7b61b5a8ce742f1a46f96f96f568230
ryantanyx <ryantanyx@gmail.com>
2022-02-11 13:28:11 +0800

    fixed exception in android connect

--- RPI/main.py
+++ RPI/main.py
@@ -114,7 +114,6 @@ class RPI(threading.Thread):
             if androidMsg:
                 print("Message from android: " + androidMsg)
                 if androidMsg.upper() == "W":
-                    print("Move forward")
                     tmp = input("Message please: ")
                     self.sendToAndroid(tmp)
 

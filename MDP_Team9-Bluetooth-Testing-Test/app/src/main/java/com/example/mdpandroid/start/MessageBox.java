64ff2e12cde85b402402d11f5858cc67a0137f26
LH <p.lianghong@gmail.com>
2022-02-23 16:55:33 +0800

    test receive

--- MDP_Team9-Bluetooth-Testing-Test/app/src/main/java/com/example/mdpandroid/start/MessageBox.java
+++ MDP_Team9-Bluetooth-Testing-Test/app/src/main/java/com/example/mdpandroid/start/MessageBox.java
@@ -20,6 +20,7 @@ import com.example.mdpandroid.R;
 import com.example.mdpandroid.map.BoardMap;
 import java.nio.charset.Charset;
 import com.example.mdpandroid.leaderboard.TimerDialogFragment;
+import com.example.mdpandroid.map.Target;
 
 public class MessageBox extends Fragment {
     private static final String TAG = "TARGET MAP";
@@ -99,44 +100,63 @@ public class MessageBox extends Fragment {
             String message = intent.getStringExtra("receivedmessage");
 //            refreshMessageReceivedfromblue();
             Log.d("summit", "ye");
-//            refreshMessageReceived();
-            String[] parts = message.split(",");
-            String type = parts[0];
-            int index;
-            switch (type) {
-                case "TARGET": {
-                    Log.d(TAG, "Interpreting TARGET message");
-                    try {
-                        String obsID = parts[1];
-                        String targetID = parts[2];
-                        if (targetID.contains("TARGET"))
-                            if (targetID.indexOf("T") != 0) {
-                                index = targetID.indexOf("T");
-                                targetID = targetID.substring(0, index);
-                            }
-                        //TODO code to set target ID from obstacle ID
-
-                    } catch (Exception e) {
-                        Log.d(TAG, "Invalid message");
-                        e.printStackTrace();
-                    }
+            refreshMessageReceived();
+//            String[] parts = message.split(",");
+//            String type = parts[0];
+//            int index;
+//            switch (type) {
+            int j;
+            String[] cases = {"TARGET", "ROBOT", "INSTRUCTIONS"};
+            String[] parts;
+            for(j = 0; j < cases.length; j++)
+                if(message.contains(cases[j]))
+                    break;
+            switch(j) {
+                case 0: {
+//                    Log.d(TAG, "Interpreting TARGET message");
+//                    try {
+//                        String obsID = parts[1];
+//                        String targetID = parts[2];
+//                        if (targetID.contains("TARGET"))
+//                            if (targetID.indexOf("T") != 0) {
+//                                index = targetID.indexOf("T");
+//                                targetID = targetID.substring(0, index);
+//                            }
+//                        //TODO code to set target ID from obstacle ID
+//
+//                    } catch (Exception e) {
+//                        Log.d(TAG, "Invalid message");
+//                        e.printStackTrace();
+//                    }
+                    parts = message.replace(" ","").replace("TARGET", "").split(",");
+                    int targetid = Integer.parseInt(parts[0]);
+                    int imageid = Integer.parseInt(parts[1]);
+                    Target t = _map.getTargets().get(targetid-1);
+                    t.setImg(imageid);
+                    break;
                 }
-                case "ROBOT": {
-                    Log.d(TAG, "Interpreting ROBOT message");
-                    try {
-                        int x = Integer.parseInt(parts[1]);
-                        int y = Integer.parseInt(parts[2]);
-                        String direction = parts[3];
-                        //TODO set robot location based on input location
-                    } catch (Exception e) {
-                        Log.d(TAG, "Invalid message");
-                        e.printStackTrace();
-                    }
+                case 1: {
+//                    Log.d(TAG, "Interpreting ROBOT message");
+//                    try {
+//                        int x = Integer.parseInt(parts[1]);
+//                        int y = Integer.parseInt(parts[2]);
+//                        String direction = parts[3];
+//                        //TODO set robot location based on input location
+//                    } catch (Exception e) {
+//                        Log.d(TAG, "Invalid message");
+//                        e.printStackTrace();
+//                    }
+                    parts = message.replace(" ","").replace("ROBOT", "").split(",");
+                    _map.getRobo().setX(Integer.parseInt(parts[0]));
+                    _map.getRobo().setY(20-Integer.parseInt(parts[1]));
+                    _map.getRobo().setFacing(Integer.parseInt(parts[2])); //0123 NSEW
+                    break;
                 }
-                case "INSTRUCTIONS": {
+                case 2: {
                     Log.d(TAG, "Interpreting ROBOT message");
+                    parts = message.split(",");
                     messageReceivedTextView.setText(parts.toString());
-
+                    break;
                 }
                 default: {
                     Log.d(TAG, "Invalid format: unable to recognize message type");
